85aa9bf63de8048bc4212e9d7302bf2cf901993a
Ardizzone Pietro <ardizzone.pietro@osdbsesto>
2022-03-03 11:13:44 +0100

    connection to db

--- back/index.js
+++ back/index.js
@@ -3,7 +3,7 @@ var apiServer = express();
 var cors = require("cors");
 apiServer.use(cors());
 var fs = require("fs")
-
+let mysql = require ('mysql');
 
 var host = "localhost";
 var port = 3000;
@@ -11,29 +11,66 @@ var port = 3000;
 apiServer.listen(port, host, () => {
   console.log("Server partito: http://%s:%d/", host, port);
 });
+var connection = mysql.createConnection ({
+  host: "ardizzone.pietro.tave.osdb.it",
+  user: "c167_pietro",
+  password: 'Az-24588',
+  database: 'c167_node'
+  });
+  connection.connect(function(error){
+   if(!!error){
+    console.log('error: '+error)
+   }else{
+     console.log('connesso al db')
+   }
+  })
 
-
+ 
+  
 
 apiServer.get("/api/login", (req, res) => {
+ 
   console.log("ricevuti:", req.query.mail, req.query.password);
-  var let = "";
-  fs.readFile("back/users.json", "utf8", (err, jsonString) => {
-    if (err) {
-        console.log("errore nella lettura del file:", err);
-        return;
+  var a = connection.query("SELECT * FROM utenti", (err, rows)=>{
+    if(!err){
+      console.log("ci");
+      var x = 0;
+      
+        for (let index = 0; index < rows.length; index++) {
+     console.log("m: "+rows[index].mail+" pw: "+rows[index].pw)
+          if(rows[index].mail==req.query.mail ){
+         
+           
+              console.log("oa")
+              x=1
+              res.status(200).json({"message":"login effettuato"});
+            
+            
+          }
+          
+        }
+        if(x==0){
+          res.status(400).json({"message":"login fallito"});      
+        }
+    
     }
-   let = JSON.parse(jsonString)
-   console.log("c: "+let)
-   for (let index = 0; index < let.length; index++) {
-     console.log(let[index].user+" "+let[index].pw)
-    if(req.query.mail===let[index].user && req.query.password===let[index].pw){
-      res.status(200).json({"message":"login effettuato"});
-    }else{
-      res.status(400).json({"message":"login fallito"});
-    }
-    }
-     
-   
   })
-  
 });
+
+apiServer.get("/api/reg", (req, res) =>{
+
+  console.log("ricevuti:", req.query.mail, req.query.password);
+  var sql = "INSERT INTO utenti (mail, pw) VALUES ('"+req.query.mail+"', '"+req.query.password+"')";
+  connection.query(sql, function (err, result) {
+    if (err) {
+      console.log("err: "+err)
+      res.status(400).json({"message":"inserimentyo fallito"});      
+    }else{
+      res.status(200).json({"message":"login effettuato"});
+      console.log("1 record inserted");
+    }
+   
+  });
+ 
+})
+  
